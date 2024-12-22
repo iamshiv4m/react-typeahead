f190447d33fea1425062e81dd22bd8c6878ca758
shivam.jha1 <shivam.jha@pw.live>
2024-12-23 00:20:02 +0530

    chore: bump version to 1.0.6 and update rollup configuration with new plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,11 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { createRequire } from "module";
-import { visualizer } from "rollup-plugin-visualizer";
 import terser from "@rollup/plugin-terser";
-import cssnano from "cssnano";
-import gzip from "rollup-plugin-gzip";
 
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
@@ -39,30 +36,14 @@ export default [
       typescript({
         tsconfig: "./tsconfig.json",
         exclude: ["**/*.test.tsx", "**/*.test.ts", "examples/**/*"],
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: true,
-            sourceMap: false,
-          },
-        },
+        declaration: false,
+        sourceMap: false,
       }),
       postcss({
         extract: false,
         minimize: true,
         modules: true,
         namedExports: true,
-        plugins: [
-          cssnano({
-            preset: [
-              "default",
-              {
-                discardComments: { removeAll: true },
-                normalizeWhitespace: true,
-                minifyFontValues: true,
-              },
-            ],
-          }),
-        ],
       }),
       terser({
         compress: {
@@ -71,13 +52,6 @@ export default [
           passes: 2,
         },
       }),
-      visualizer({
-        filename: "bundle-analysis.html",
-        gzipSize: true,
-        brotliSize: true,
-        sourcemap: true,
-        open: true,
-      }),
       {
         treeshake: {
           moduleSideEffects: false,
@@ -85,10 +59,6 @@ export default [
           tryCatchDeoptimization: false,
         },
       },
-      gzip({
-        filter: /\.(js|css|html|json|ico|svg)$/,
-        minSize: 1024,
-      }),
     ],
     external: ["react", "react-dom"],
   },
@@ -96,7 +66,15 @@ export default [
     input: "src/index.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [
-      dts(),
+      dts({
+        respectExternal: true,
+        compilerOptions: {
+          baseUrl: ".",
+          paths: {
+            "*": ["*"],
+          },
+        },
+      }),
       {
         name: "ignore-css-imports",
         transform(code, id) {
